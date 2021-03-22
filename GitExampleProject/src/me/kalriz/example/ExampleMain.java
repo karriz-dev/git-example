97a3452ce3eb7e623eb96406c49795309f680d6f
covision <covision@10.10.32.101>
2021-03-22 11:18:14 +0900

    first project upload

---
+++ GitExampleProject/src/me/kalriz/example/ExampleMain.java
@@ -0,0 +1,10 @@
+package me.kalriz.example;
+
+public class ExampleMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Git Test");
+	}
+
+}
